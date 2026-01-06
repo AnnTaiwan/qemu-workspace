0b73b032338a51eb83cf87020fba77e600cd9077
Ann <ann20021007@gmail.com>
2026-01-06 17:22:55 +0800

    use variables to represent the img_name and iso_name

--- build_images/create_image.sh
+++ build_images/create_image.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Reference: https://qemu-project.gitlab.io/qemu/system/images.html
 # https://www.gem5.org/documentation/general_docs/fullsystem/disks
-qemu-img create x86-ubuntu-14.04.6.img 4G
+IMAGE="x86-ubuntu-14.04.6.img"
+ISO="ubuntu-14.04.6-server-amd64.iso"
+qemu-img create $IMAGE 4G
 qemu-system-x86_64 \
-    -hda x86-ubuntu-14.04.6.img \
-    -cdrom ubuntu-14.04.6-server-amd64.iso \
+    -hda $IMAGE \
+    -cdrom $ISO \
     -m 1024 \
     -enable-kvm \
     -boot d
 # Install Ubuntu
-qemu-system-x86_64 -hda x86-ubuntu-14.04.6.img -cdrom ubuntu-14.04.6-server-amd64.iso -m 1024  -enable-kvm -boot d
+qemu-system-x86_64 -hda $IMAGE -cdrom $ISO -m 1024 -enable-kvm -boot d
 # Use it without -boot d
-qemu-system-x86_64 -hda x86-ubuntu-14.04.6.img -m 1024 -enable-kvm -serial mon:stdio
+qemu-system-x86_64 -hda $IMAGE -m 1024 -enable-kvm -serial mon:stdio
