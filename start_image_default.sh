b82fd8fff6d3552ce6515aac01d1125d15f2c99f
Ann <ann20021007@gmail.com>
2026-02-02 14:05:25 +0800

    Turn into executable file, add image-selection in shell

--- start_image_default.sh
+++ start_image_default.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+IMAGE=${1:-"disk_images/x86-ubuntu-14.04.6_v1.img"}
 qemu-system-x86_64 \
     -enable-kvm \
     -cpu host \
     -m 16G \
     -smp 8 \
-    -hda disk_images/x86-ubuntu-14.04.6_v1.img \
+    -hda $IMAGE \
     -net nic -net user \
     -serial mon:stdio \
